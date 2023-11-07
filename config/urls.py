b400512eaaa4d0035c191b0c56011f6e9a034891
Clara Borlu <claraborlu24@gmail.com>
2023-11-07 16:54:03 +0000

    Add: swagger UI

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+openapi.Info(
+title="Todo App API",
+default_version="v1",
+description="BUilding A Todo App",
+terms_of_service="TodoApp.com",
+contact=openapi.Contact(email="claraborlu24@gmail.com"),
+license=openapi.License(name="BSD License"),
+
+),
+public=True,
+permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls'),)
+    path('accounts/', include('accounts.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
